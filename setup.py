9da87e0b338cb5004f8811c1700964b6c1e3422c
Thurloat <thurloat@gmail.com>
2013-01-16 22:20:06 -0400

    pip installable!

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = [
+    'django>=1.2',
+]
+
+f = open('README.rst')
+readme = f.read()
+f.close()
+
+setup(
+    name='django-postleware',
+    version='1.0',
+    author='Adam Thurlow',
+    author_email='thurloat@gmail.com',
+    url='http://github.com/thurloat/django-postleware',
+    description='A simple django middleware that ensures POSTs aren\'t cached by clients.',
+    long_description=readme,
+    license='BSD',
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Software Development'
+    ],
+)
