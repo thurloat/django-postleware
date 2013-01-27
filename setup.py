0a1ecfe9d017781d1f6a1af842a5905406c55857
Thurloat <thurloat@gmail.com>
2013-01-27 14:22:12 -0400

    version bump!

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ f.close()
 
 setup(
     name='django-postleware',
-    version='1.0',
+    version='1.0.1',
     author='Adam Thurlow',
     author_email='thurloat@gmail.com',
     url='http://github.com/thurloat/django-postleware',
