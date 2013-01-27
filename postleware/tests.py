2d2e360291d1f4aabaec655d56970b628f0b3a7e
Karl Leuschen <karl.leuschen@sheepdoginc.ca>
2013-01-27 10:52:59 -0400

    Add tests for GET requests and for requests with header already set
    Test for GET required updates to test view, so that the request could
    be handled.

--- postleware/tests.py
+++ postleware/tests.py
@@ -1,11 +1,40 @@
+from django.http import HttpResponse
 from django.utils import unittest
 from django.test.client import Client
+from postleware import PostResponseCachebusterMiddleware
+
 
 class PostResponseMiddleware(unittest.TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_basic_response(self):
-        response = self.client.post('/test1', {'foo': 'bar'})
+    def test_header_added_when_necessary(self):
+        # 'Cache-Control: no-cache' is added to POSTs
+        response = self.client.post('/test1', {'foo':'bar'})
         self.assertEqual(response['Cache-Control'], 'no-cache')
 
+        # 'Cache-Control' is NOT added to GETs
+        response = self.client.get('/test1')
+        self.assertFalse(response.has_header('Cache-Control'))
+
+    def test_header_not_added_when_present(self):
+        middleware = PostResponseCachebusterMiddleware()
+        test_header_setting = 'test-setting'
+        raw_response = HttpResponse()
+
+        # 'Cache-Control' header isn't modified when present on POSTs
+        request = MockRequest('POST')
+        raw_response['Cache-Control'] = test_header_setting
+        response = middleware.process_response(request, raw_response)
+        self.assertEqual(response['Cache-Control'], test_header_setting)
+
+        # 'Cache-Control' header isn't modified when present on GETs
+        request = MockRequest('GET')
+        raw_response['Cache-Control'] = test_header_setting
+        response = middleware.process_response(request, raw_response)
+        self.assertEqual(response['Cache-Control'], test_header_setting)
+
+
+class MockRequest(object):
+    def __init__(self, method=None):
+        self.method = method
--- postleware/views.py
+++ postleware/views.py
@@ -2,5 +2,11 @@ from django.http import HttpResponse
 from django.views.generic import View
 
 class TestView(View):
-    def post(self, request, *args, **kwargs):
+    def __init__(self, **kwargs):
+        super(TestView, self).__init__(**kwargs)
+
+        for method in self.http_method_names:
+            setattr(self, method, self._default_response)
+
+    def _default_response(self, request):
         return HttpResponse('testing!')
